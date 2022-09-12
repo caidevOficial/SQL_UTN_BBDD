c43cf1d2788a67a1f3189085472bf44b6711b2f4
[FacuFalcone] <ffalcone@fra.utn.edu.ar>
2022-09-12 20:22:30 -0300

    Upg :recycle: - Queries Syntax
    (cherry picked from commit eb9dc4ef17e21269159603448e861b4e4aa17980)

--- Clase_02/DML/Inserts/ejemplos.prov-prod.sql
+++ Clase_02/DML/Inserts/ejemplos.prov-prod.sql
@@ -1,8 +1,27 @@
-CREATE TABLE IF NOT EXISTS `ejemplos`.`PROV-PROD`(
-    `NUMERO` INT NOT NULL,
-    `PNRO` INT NOT NULL,
-    `CANTIDAD` INT NOT NULL,
-    PRIMARY KEY (`NUMERO`, `PNRO`),
-    FOREIGN KEY (`NUMERO`) REFERENCES PROVEEDORES(`NUMERO`),
-    FOREIGN KEY (`PNRO`) REFERENCES PRODUCTOS (`PNRO`)
-);
+-- MIT License
+-- 
+-- Copyright (c) 2022 [FacuFalcone]
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+INSERT INTO `ejemplos`.`PROV-PROD` VALUES
+(101, 001, 20) ,(101, 002, 20) ,(101, 003, 20) ,(102, 004, 20)
+,(102, 003, 20) ,(103, 004, 20) ,(103, 005, 20) ,(103, 006, 20)
+,(104, 001, 20) ,(104, 002, 20) ,(104, 003, 20) ,(105, 004, 20)
+,(105, 005, 20) ,(105, 006, 20) ,(106, 002, 20) ,(106, 004, 20);
