eb9dc4ef17e21269159603448e861b4e4aa17980
[FacuFalcone] <ffalcone@fra.utn.edu.ar>
2022-09-12 20:22:30 -0300

    Upg :recycle: - Queries Syntax

--- Clase_02/DML/Inserts/ejemplos.proveedores.sql
+++ Clase_02/DML/Inserts/ejemplos.proveedores.sql
@@ -1,7 +1,29 @@
-CREATE TABLE IF NOT EXISTS `ejemplos`.`PROVEEDORES`(
-    `NUMERO` INT NOT NULL,
-    `NOMBRE` VARCHAR(20) NOT NULL,
-    `DOMICILIO` VARCHAR(15) NOT NULL,
-    `LOCALIDAD` VARCHAR(12),
-    PRIMARY KEY (`NUMERO`)
-);
+-- MIT License
+-- 
+-- Copyright (c) 2022 [FacuFalcone]
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+INSERT INTO `ejemplos`.`PROVEEDORES` VALUES
+(1, 'Proveedor 1', 'Calle Falsa 123', 'Capital')
+,(2, 'Proveedor 2', 'Calle Falsa 456', 'Avellaneda')
+,(3, 'Proveedor 3', 'Calle Falsa 789', 'Berazategui')
+,(4, 'Proveedor 4', 'Calle Falsa 1011', 'Capital')
+,(5, 'Proveedor 5', 'Calle Falsa 1213', 'Lanus')
+,(6, 'Proveedor 6', 'Calle Falsa 1415', 'Berazategui');
